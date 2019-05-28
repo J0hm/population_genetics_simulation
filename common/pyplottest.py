edc071b846c0ae3a7bc68290ab170c6e57882a0e
J0hm <johnkerr2003@gmail.com>
2019-05-28 08:48:46 -0400

    numpy array solution

--- common/pyplottest.py
+++ common/pyplottest.py
@@ -1,2 +1,19 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+graph = plt.figure()
+graph, ax_lst = plt.subplots(1, 1)
+
+array = np.array([[1, 2, 3], [.5, .7, .9], [.4, .5, .6], [.8, .9, 1.0]])
+
+# Implement this in genetics.py
+def addNPGen(array, gen, avgFit, minFit, maxFit):
+    return np.append(array, [[gen], [avgFit], [minFit], [maxFit]], axis=1)
+
+
+array = addNPGen(array, 4, .7, .6, .9)
+
+
+print(array)
+
